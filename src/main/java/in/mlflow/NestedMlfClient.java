1947ae032e75ae1c96180f396bb5d375cfaf39ee
algo <alghoshal@gmail.com>
2025-10-26 12:48:09 +0530

    Two levels nesting

--- src/main/java/in/mlflow/NestedMlfClient.java
+++ src/main/java/in/mlflow/NestedMlfClient.java
@@ -1,14 +1,21 @@
 package in.mlflow;
 
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-
 import org.mlflow.tracking.ActiveRun;
 import org.mlflow.tracking.MlflowClient;
 import org.mlflow.tracking.MlflowContext;
 
 /**
- * NestedMlflowClient showing Child runs nested within Parent run
+ * NestedMlflowClient showing two level child/ grand-child runs nested within Parent run.
+ * 
+ * Creates run hierarchy of the form:
+ * 
+ * 	Parent
+ * 		|
+ * 		|----- Child1
+ *		| 			|
+ * 		|			---- GrandChild1
+ * 		|
+ * 		|---- Child2
  * 
  * @author algo
  */
@@ -28,7 +35,7 @@ public class NestedMlfClient {
 
 		NestedMlfClient mlfClient = new NestedMlfClient();
 
-		mlfClient.captureActiveRun(mlflowContext, experimentName, "parent");
+		mlfClient.captureActiveRun(mlflowContext, experimentName, "Parent");
 
 
 	}
@@ -45,13 +52,20 @@ public class NestedMlfClient {
 			run.logMetric("PARENT_METRIC", 20.0);
 			
 			// Start a nested child run
-			ActiveRun childRun1 = mlflowContext.startRun("child1", run.getId());
+			ActiveRun childRun1 = mlflowContext.startRun("Child1", run.getId());
 			childRun1.logParam("child1_dat1" , "c1");
 			childRun1.logMetric("CHILD1_METRIC", 3);
+			
+			// Start a two levels nested, grand child run
+			ActiveRun grandchildRun1 = mlflowContext.startRun("GrandChild1", childRun1.getId());
+			grandchildRun1.logParam("grandchild1_dat1" , "grnc1");
+			grandchildRun1.logMetric("grandchild1_METRIC", 30303);
+			
+			// End child1 run
 			childRun1.endRun();
 			
 			// Start another nested child run
-			ActiveRun childRun2 = mlflowContext.startRun("child2", run.getId());
+			ActiveRun childRun2 = mlflowContext.startRun("Child2", run.getId());
 			childRun2.logParam("child2_dat2" , "c2");
 			childRun2.logMetric("CHILD2_METRIC", 131);
 			childRun2.endRun();
